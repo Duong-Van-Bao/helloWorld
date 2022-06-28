83bcaabad0f72e330ef9bcaa1b02ce23872e3c5c
Duong_Van_Bao <duongbao9109@gmail.com>
2022-06-28 22:30:11 +0700

    fix bug

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,8 +1,16 @@
 
+/*
+ * Mục đích: luyện tập chương trình đầu tiên
+ * Người tạo: CyberSort
+ * Ngày tạo: 16/2/2020 
+ * */
 public class MainClass {
-
 	public static void main(String[] args) {
-      System.out.println("Hello world");
+      System.out.println("=========***======= \n Lorem Ipsum is simply dummy text of the printing and typesetting industry. \n Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. \n It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. \n It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+      /*
+       * Bài tập 2:
+       * */
+      System.out.println("Xin chào \'CyberSort\'");
+      System.out.println("STT \t|Họ Tên \t Ngày sinh \t |Nơi sinh");	
 	}
-
 }
